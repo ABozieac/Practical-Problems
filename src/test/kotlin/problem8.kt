082710900daf5d63564765ec9fef8290ef25f054
Artiom Bozieac <artiom.bozieac@crunchyroll.com>
2022-04-19 17:03:00 +0300

    Added solution for problem8

---
+++ src/test/kotlin/problem8.kt
@@ -0,0 +1,20 @@
+fun main() {
+    println(formatTrainRoute(listOf("Station1", "Station2", "Station3")))
+}
+
+fun formatTrainRoute(stations: List<String>): String {
+    var result = "Train is calling at"
+
+    stations.forEachIndexed { index, station ->
+        if (index == stations.size - 1 && index == 0)
+            result += " $station"
+        else if (index == stations.size - 1)
+            result += " and $station"
+        else if (index == stations.size - 2)
+            result += " $station"
+        else
+            result += " $station,"
+    }
+
+    return result
+}
