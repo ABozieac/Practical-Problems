0642b4ef9d7edba2aa622299c96cf87c8acd8f2b
Artiom Bozieac <artiom.bozieac@crunchyroll.com>
2022-04-19 17:13:16 +0300

    Added solution for problem17

---
+++ src/test/kotlin/problem17.kt
@@ -0,0 +1,15 @@
+fun main() {
+    println(longestWord("this is a house"))
+}
+
+fun longestWord(str: String): String {
+    var words = str.split(" ")
+    var biggestWord = words[0]
+
+    for (word in words) {
+        if (word.length > biggestWord.length)
+            biggestWord = word
+    }
+
+    return biggestWord
+}
