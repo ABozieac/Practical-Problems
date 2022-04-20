4be90d144b9aafa2d7d056b76189d94c9729b9a5
Artiom Bozieac <artiom.bozieac@crunchyroll.com>
2022-04-20 14:58:51 +0300

    Fixed the solution when other characters than letters appeared

--- src/test/kotlin/problem26.kt
+++ src/test/kotlin/problem26.kt
@@ -1,12 +1,15 @@
 fun main() {
-    println(encodeCaesarCipher("abc", 1))
+    println(encodeCaesarCipher("xyz", 1))
 }
 
 fun encodeCaesarCipher(str: String, shift: Int): String {
     var result = ""
 
     for (i in str.indices) {
-        result += (str[i].code + shift).toChar()
+        if(!(str[i].code + shift).toChar().isLetter())
+            result += (96 + shift).toChar()
+        else
+            result += (str[i].code + shift).toChar()
     }
 
     return result
