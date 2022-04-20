d6ef3157ab2411faddb85c022f472eca9feb6256
Artiom Bozieac <artiom.bozieac@crunchyroll.com>
2022-04-20 14:23:44 +0300

    Used collections methods instead of looping through

--- src/test/kotlin/problem10.kt
+++ src/test/kotlin/problem10.kt
@@ -1,17 +1,9 @@
+import java.util.*
+
 fun main() {
-    println(countUniqueValues(listOf(-2, -1, -1, 0, 1)))
+    println(countUniqueValues(listOf(-2, -1, 0, -1, 2, 3)))
 }
 
 fun countUniqueValues(nums: List<Int>): Int {
-    var result = 0
-
-    for (i in nums.indices) {
-        if (i == nums.size - 1 && nums[i] != nums[i - 1]) {
-            result++
-        } else if (nums[i] != nums[i + 1]) {
-            result++
-        }
-    }
-
-    return result
+    return nums.count() { Collections.frequency(nums, it) == 1 }
 }
