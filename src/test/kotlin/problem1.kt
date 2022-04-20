f5bd039f3426c3eda4044c75aede38aa8fb39002
Artiom Bozieac <artiom.bozieac@crunchyroll.com>
2022-04-20 13:39:58 +0300

    Fixed solution for problem1

---
+++ src/test/kotlin/problem1.kt
@@ -0,0 +1,19 @@
+fun main() {
+    val str = "test,} for, mfunction"
+    println(capitalizeSentences(str))
+}
+
+fun capitalizeSentences(str: String): String {
+    var result = StringBuilder()
+
+    for (i in str.indices) {
+        if (str[i].isLetter() && i == 0 || str[i].isLetter() && !str[i - 1].isLetter())
+            result.append(str[i].uppercaseChar())
+        else if (str[i].isLetter())
+            result.append(str[i].lowercaseChar())
+        else
+            result.append(str[i])
+    }
+
+    return result.toString()
+}
