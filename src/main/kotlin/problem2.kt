0ddc77e80a6ff30f0fa040ef5fa3e036fb181346
Artiom Bozieac <artiom.bozieac@crunchyroll.com>
2022-04-20 13:44:46 +0300

    Added foreach instead of for

--- src/main/kotlin/problem2.kt
+++ src/main/kotlin/problem2.kt
@@ -1,13 +1,12 @@
-fun main(){
-    println(getOdd(listOf(1,2,3,4,5,6,7,8,9)))
+fun main() {
+    println(getOdd(listOf(1, 2, 3, 4, 5, 6, 7, 8, 9)))
 }
 
-fun getOdd(nums: List<Int>) : List<Int>{
+fun getOdd(nums: List<Int>): List<Int> {
     var result = mutableListOf<Int>()
-
-    for(num in nums){
-        if(num % 2 != 0)
-            result.add(num)
+    nums.forEach { value ->
+        if (value % 2 != 0)
+            result.add(value)
     }
 
     return result
