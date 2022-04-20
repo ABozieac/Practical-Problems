c477c25f11f7cb6c555fdc449d9a8b90718844e6
Artiom Bozieac <artiom.bozieac@crunchyroll.com>
2022-04-20 14:44:18 +0300

    Added trim() to each element

--- src/test/kotlin/problem15.kt
+++ src/test/kotlin/problem15.kt
@@ -1,13 +1,13 @@
 fun main() {
-    println(capitalizeFirst(listOf("igor", "wojda")))
+    println(capitalizeFirst(listOf("igor", " wojda", "loh")))
 }
 
 fun capitalizeFirst(strings: List<String>): List<String> {
     var result = strings.toMutableList()
 
     for (i in strings.indices) {
-        if (!((strings[i])[0].isUpperCase())) {
-            result[i] = "${strings[i][0].uppercaseChar()}${strings[i].substring(1)}"
+        if (!((strings[i].trim())[0].isUpperCase())) {
+            result[i] = "${strings[i].trim()[0].uppercaseChar()}${strings[i].trim().substring(1)}"
 
             if (i == strings.size - 1)
                 return result
