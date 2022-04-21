c9fdd1cf083ec92b51ba2d6f97ffe7cb2f9181af
Artiom Bozieac <artiom.bozieac@crunchyroll.com>
2022-04-21 19:32:38 +0300

    Changed to older solution

--- src/test/kotlin/problem.kt
+++ src/test/kotlin/problem.kt
@@ -1,13 +1,22 @@
 fun main() {
-    println(anagrams("rail safety", "fairy tales"));
+    println(anagrams("rail saflty", "fairy tales"));
 }
 
-fun anagrams(str1: String, str2: String): Boolean {
-    for(ch in str2){
-        if(ch.isLetter() && !str1.contains(ch)){
-            return false
+fun anagrams(_str1: String, _str2: String): Boolean {
+    var str1 = ""
+    var str2 = ""
+
+    for (i in _str1.indices) {
+        if (_str1[i].isLetter()) {
+            str1 += _str1[i].lowercaseChar()
         }
     }
 
-    return true
+    for (i in _str2.indices) {
+        if (_str2[i].isLetter()) {
+            str2 += _str2[i].lowercaseChar()
+        }
+    }
+
+    return (str1.toCharArray().sorted() == str2.toCharArray().sorted())
 }
