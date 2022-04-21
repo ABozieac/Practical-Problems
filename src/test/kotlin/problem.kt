be59f82ec454c22ff986e0e81e5db3a74eff62bf
Artiom Bozieac <artiom.bozieac@crunchyroll.com>
2022-04-21 21:02:31 +0300

    Used regex

--- src/test/kotlin/problem.kt
+++ src/test/kotlin/problem.kt
@@ -1,42 +1,23 @@
+import java.util.regex.Matcher
+import java.util.regex.Pattern
+
 fun main() {
-    println(decapitalizeConst("_FOO_BAR_BAZ"))
+    println(decapitalizeConst("__FOO_BAR_BAZ"))
 }
 
 fun decapitalizeConst(str: String): String {
-    var result = ""
+    var result = StringBuilder()
 
-    var i = 0
-    var flag: Boolean
-    var hasDoubleUnderscore = false
+    var matches = Regex("[a-zA-Z]+").findAll(str)!!
 
-    while (i != str.length) {
-        flag = false
-        if (str[i] == '_') {
-            for (j in i + 1..str.length - 1) {
-                if (str[j] != '_') {
-                    if (result.length == 0)
-                        result += str[j].lowercaseChar()
-                    else
-                        result += str[j].uppercaseChar()
+    for (match in matches)
+        if (match.value == matches.elementAt(0).value)
+            result.append(match.value.lowercase())
+        else
+            result.append(match.value[0].uppercaseChar() + match.value.substring(1, match.value.length).lowercase())
 
+    if ("_{2}".toRegex().containsMatchIn(str))
+        result.append("lTH")
 
-                    i = j
-                    flag = true
-                    break;
-                } else {
-                    hasDoubleUnderscore = true
-                }
-            }
-        }
-
-        if (!flag)
-            result += str[i].lowercaseChar()
-
-        i++
-    }
-
-    if (hasDoubleUnderscore)
-        result += "lTH"
-
-    return result
+    return result.toString()
 }
