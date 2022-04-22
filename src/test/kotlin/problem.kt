ed27120498b8a38192b208a16fdc5ddd29fbc4b1
Artiom Bozieac <artiom.bozieac@crunchyroll.com>
2022-04-22 23:17:27 +0300

    Added comments

--- src/test/kotlin/problem.kt
+++ src/test/kotlin/problem.kt
@@ -2,6 +2,11 @@ fun main() {
     println(getOdd(listOf(1, 2, 3, 4, 5, 6, 7, 8, 9)))
 }
 
+/**
+ * Get odd numbers from a list
+ * @param nums - A list of integers
+ * @return A list of integers containing only odd numbers
+ */
 fun getOdd(nums: List<Int>): List<Int> {
     var result = mutableListOf<Int>()
     nums.forEach { value ->
