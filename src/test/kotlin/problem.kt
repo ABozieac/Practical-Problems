ccb90a881945a3998f106254862e7f56e017aeca
Artiom Bozieac <artiom.bozieac@crunchyroll.com>
2022-04-22 23:36:44 +0300

    Added comments

--- src/test/kotlin/problem.kt
+++ src/test/kotlin/problem.kt
@@ -2,6 +2,12 @@ fun main() {
     println(getIndex(listOf("A", "B", "C"), "E"))
 }
 
+/**
+ * Gets the index of a string where its met in the list
+ * @param list - List with strings
+ * @param str - String to search for
+ * @return the index where str is met in list
+ */
 fun getIndex(list: List<String>, str: String): Int {
     for (i in list.indices)
         if (list[i] == str)
