77505a083f73522fd8ab6162f89a0dabbd634366
Artiom Bozieac <artiom.bozieac@crunchyroll.com>
2022-04-22 23:43:12 +0300

    Added comments

--- src/test/kotlin/problem.kt
+++ src/test/kotlin/problem.kt
@@ -4,16 +4,25 @@ fun main() {
     println(formatTrainRoute(listOf("Station1", "Station2", "Station3")))
 }
 
+/**
+ * Creates train route
+ * @param stations - List of stations
+ * @return A string with the route of train
+ */
 fun formatTrainRoute(stations: List<String>): String {
     var result = "Train is calling at"
 
     stations.forEachIndexed { index, station ->
+        // If only one station add " station"
         if (index == stations.size - 1 && index == 0)
             result += " $station"
+        // Elif last station add " and station"
         else if (index == stations.size - 1)
             result += " and $station"
+        // Elif station before last add " station"
         else if (index == stations.size - 2)
             result += " $station"
+        // Else separate stations with comma
         else
             result += " $station,"
     }
