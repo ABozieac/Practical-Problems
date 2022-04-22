a9327c819453f56375b880cae7b3f6305622b6a1
Artiom Bozieac <artiom.bozieac@crunchyroll.com>
2022-04-22 22:47:57 +0300

    Added comments

--- src/test/kotlin/problem.kt
+++ src/test/kotlin/problem.kt
@@ -5,15 +5,22 @@ fun main() {
     println(capitalizeSentences(str))
 }
 
+/**
+ * Capitalizes every word in a sentence
+ * @param str - Sentence to capitalize words
+ */
 fun capitalizeSentences(str: String): String {
     var result = StringBuilder()
 
     for (i in str.indices) {
-        if (str[i].isLetter() && i == 0 || str[i].isLetter() && !str[i - 1].isLetter() && str[i-1] != '-')
+        // If first letter of word or letter after symbols then capitalize it
+        if (str[i].isLetter() && i == 0 || str[i].isLetter() && !str[i - 1].isLetter() && str[i - 1] != '-')
             result.append(str[i].uppercaseChar())
+        // Elif not first letter but capitalized then make it lowercase
         else if (str[i].isLetter() && i != 0 && !str[i].isUpperCase())
             result.append(str[i].lowercaseChar())
         else
+        // Remove multiple empty spaces
             if (i - 1 > 0 && str[i - 1] != ' ')
                 result.append(str[i])
     }
