f63ebfa7b6d546a4fbfa10bef77f4c1b1458aa49
Artiom Bozieac <artiom.bozieac@crunchyroll.com>
2022-04-22 22:56:15 +0300

    Added comments

--- src/test/kotlin/problem.kt
+++ src/test/kotlin/problem.kt
@@ -6,6 +6,12 @@ fun main() {
     println(power(200000, 200))
 }
 
+/**
+ * Raise a to the power of b
+ * @param a - base
+ * @param b - exponent
+ * @return value of a raised to the power o b
+ */
 fun power(a: Int, b: Int): BigInteger {
     var result = BigInteger("1")
 
