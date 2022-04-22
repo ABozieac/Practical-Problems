3c819cef08c65e2152b872eba9f7d12a35ff262c
Artiom Bozieac <artiom.bozieac@crunchyroll.com>
2022-04-22 22:50:15 +0300

    Added comments

--- src/test/kotlin/problem.kt
+++ src/test/kotlin/problem.kt
@@ -1,11 +1,13 @@
-import java.util.*
-
 //Comment for commit
 
 fun main() {
     println(countUniqueValues(listOf(-2, -1, 0, -1, 2, 3)))
 }
 
+/**
+ * Counts unique values in a list of numbers
+ * @param nums - A list of integers
+ */
 fun countUniqueValues(nums: List<Int>): Int {
     return nums.distinct().size
 }
