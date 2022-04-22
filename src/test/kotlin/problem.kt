68d1dbd8a589ead5c2e5cd32eb3a0854ee863e32
Artiom Bozieac <artiom.bozieac@crunchyroll.com>
2022-04-22 23:06:37 +0300

    Added comments

--- src/test/kotlin/problem.kt
+++ src/test/kotlin/problem.kt
@@ -4,18 +4,27 @@ fun main() {
     println(capitalizeFirst(listOf("igor", " wojda", "loh")))
 }
 
+/**
+ * Capitalizes strings in a list using recursion
+ * @param strings - a list of strings
+ * @return A list of capitalized strings
+ */
 fun capitalizeFirst(strings: List<String>): List<String> {
     var result = strings.toMutableList()
 
     for (i in strings.indices) {
+        // Trim string to avoid bugs and check if it's not capitalized
         if (!((strings[i].trim())[0].isUpperCase())) {
+            // Capitalize the string
             result[i] = "${strings[i].trim()[0].uppercaseChar()}${strings[i].trim().substring(1)}"
+
 
             if (i == strings.size - 1)
                 return result
         }
     }
 
+    // Do it for the next string using recursion
     capitalizeFirst(result)
 
     return result
