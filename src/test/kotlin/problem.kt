7f71e629db9e8bc6f5835bbe8429b424c7d0eeb4
Artiom Bozieac <artiom.bozieac@crunchyroll.com>
2022-04-22 23:32:28 +0300

    Added comments

--- src/test/kotlin/problem.kt
+++ src/test/kotlin/problem.kt
@@ -2,6 +2,12 @@ fun main(){
     println(containsRange(5..8, 5..9))
 }
 
+/**
+ * Checks if a range contains another
+ * @param range1 - first range
+ * @param range2 - second range
+ * @return Boolean if range1 contains range2 or no
+ */
 fun containsRange(range1: ClosedRange<Int>, range2: ClosedRange<Int>) : Boolean{
     return range1.start <= range2.start && range1.endInclusive >= range2.endInclusive
 }
