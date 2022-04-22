39191f4716f5d6658f3154f3b179a6f2eaa99c78
Artiom Bozieac <artiom.bozieac@crunchyroll.com>
2022-04-22 23:12:02 +0300

    Added comments

--- src/test/kotlin/problem.kt
+++ src/test/kotlin/problem.kt
@@ -4,6 +4,11 @@ fun main() {
     println(palindrome("abba"))
 }
 
+/**
+ * Checks if a word is a palindrome
+ * @param str - Word to check
+ * @return Is word a palindrome or not
+ */
 fun palindrome(str: String): Boolean {
     return str == str.reversed()
 }
