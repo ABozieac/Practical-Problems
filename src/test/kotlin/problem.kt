5e48209553f6ec2c44b7ebbbad6bef01dc0a2982
Artiom Bozieac <artiom.bozieac@crunchyroll.com>
2022-04-22 23:33:29 +0300

    Added comments

--- src/test/kotlin/problem.kt
+++ src/test/kotlin/problem.kt
@@ -2,9 +2,15 @@ fun main() {
     println(addUpTo(100))
 }
 
+/**
+ * Adds up numbers from 1 to n
+ * @param n - Upper boundary
+ * @return Sum of numbers from 1 to n
+ */
 fun addUpTo(n: Int): java.math.BigInteger {
     var result = java.math.BigInteger("0")
 
+    //Calculate the result using formula
     result += ((((1 + n) * n)) / 2).toBigInteger()
 
     return result
