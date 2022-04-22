594300b761859069052b15a55fbc6ba73ac30761
Artiom Bozieac <artiom.bozieac@crunchyroll.com>
2022-04-22 23:01:40 +0300

    Added comments

--- src/test/kotlin/problem.kt
+++ src/test/kotlin/problem.kt
@@ -4,6 +4,11 @@ fun main() {
     println(product(listOf(1, 2, 4)))
 }
 
+/**
+ * Calculates the product of all the numbers from a list
+ * @param nums - a list of integers
+ * @return product of all the numbers from the list
+ */
 fun product(nums: List<Int>): Int {
     return nums.reduce() { accumulator, element -> accumulator * element }
 }
