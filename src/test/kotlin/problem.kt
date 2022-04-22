dcdc6d58d49fd19b7cebbd7665f9547308ebd8f4
Artiom Bozieac <artiom.bozieac@crunchyroll.com>
2022-04-22 23:00:43 +0300

    Added comments

--- src/test/kotlin/problem.kt
+++ src/test/kotlin/problem.kt
@@ -4,9 +4,15 @@ fun main() {
     println(factorial(5))
 }
 
+/**
+ * Calculates the factorial of n
+ * @param n - number to calculate factorial of
+ * @return Factorial of n
+ */
 fun factorial(n: Int): Int {
     if (n == 0 || n == 1)
         return 1
 
+    //Using formula to obtain n * n-1 * n-2...
     return n * factorial(n - 1)
 }
