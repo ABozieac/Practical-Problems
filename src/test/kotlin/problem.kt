a221c020ec0ef2668d03fe8e57e1f49641ef3cfd
Artiom Bozieac <artiom.bozieac@crunchyroll.com>
2022-04-22 23:30:28 +0300

    Added comments

--- src/test/kotlin/problem.kt
+++ src/test/kotlin/problem.kt
@@ -3,6 +3,11 @@ fun main() {
     println(hasDoubleCharacter("abba"))
 }
 
+/**
+ * Checks if a string has repeated characters
+ * @param str - String to search for repeated characters
+ * @return Boolean if there are repeated characters or no
+ */
 fun hasRepeatedCharacter(str: String): Boolean {
     for (i in str.indices) {
         for (j in str.indices) {
@@ -17,6 +22,11 @@ fun hasRepeatedCharacter(str: String): Boolean {
     return false
 }
 
+/**
+ * Checks if a string has double characters, for example "aa"
+ * @param str - String to search for double characters
+ * @return Boolean if string has double characters or no
+ */
 fun hasDoubleCharacter(str: String): Boolean {
     for(i in str.indices){
         if(i+1 < str.length - 1 && str[i] == str[i+1])
