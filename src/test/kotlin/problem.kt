a172dafcfae176c3eb198fff968d962c2bf45d4b
Artiom Bozieac <artiom.bozieac@crunchyroll.com>
2022-04-22 23:31:36 +0300

    Added comments

--- src/test/kotlin/problem.kt
+++ src/test/kotlin/problem.kt
@@ -2,6 +2,12 @@ fun main() {
     println(encodeCaesarCipher("art.i.o?m;t.i'o  ma", 12))
 }
 
+/**
+ * Encodes a string to caesar cipher
+ * @param str - String to encode
+ * @param shift - Shift for the caesar cipher
+ * @return the encoded string
+ */
 fun encodeCaesarCipher(str: String, shift: Int): String {
     var result = ""
 
