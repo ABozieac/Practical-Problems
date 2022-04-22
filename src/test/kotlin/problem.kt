989ae151231d19c8276e3102f6fa740db1e26ba3
Artiom Bozieac <artiom.bozieac@crunchyroll.com>
2022-04-22 16:03:47 +0300

    Fixed the exception when using an empty string and changed var to val

--- src/test/kotlin/problem.kt
+++ src/test/kotlin/problem.kt
@@ -2,13 +2,13 @@ import java.util.regex.Matcher
 import java.util.regex.Pattern
 
 fun main() {
-    println(decapitalizeConst("_foO__bAR_BAZ_TAKE_abc"))
+    println(decapitalizeConst("__FOOBAR_ABA"))
 }
 
 fun decapitalizeConst(str: String): String {
-    var result = StringBuilder()
+    val result = StringBuilder()
 
-    var matches = Regex("[a-zA-Z]+").findAll(str)!!
+    val matches = Regex("[a-zA-Z]+").findAll(str)
 
     for (match in matches)
         if (match.value == matches.elementAt(0).value)
@@ -16,7 +16,7 @@ fun decapitalizeConst(str: String): String {
         else
             result.append(match.value[0].uppercaseChar() + match.value.substring(1, match.value.length).lowercase())
 
-    if (!str[0].isLetter())
+    if (str.isNotEmpty() && !str[0].isLetter())
         result.append("lTH")
 
     return result.toString()
