7d42275be430e178ee8bee2a68f936f0dad5bb1f
Artiom Bozieac <artiom.bozieac@crunchyroll.com>
2022-04-22 16:29:38 +0300

    Removed the useless if

--- src/test/kotlin/problem.kt
+++ src/test/kotlin/problem.kt
@@ -1,19 +1,18 @@
 //araraew
 
 fun main() {
-    println(countUpAndDown(5))
+    println(countUpAndDown(6))
 }
 
 fun countUpAndDown(n: Int): List<Int> {
     var result = mutableListOf<Int>(0, 0)
 
-    for (i in 1..n) {
+    for (i in 1 until n) {
         result.add(i, i)
         result.add(result.size - i, i)
     }
 
-    if(n != 0)
-        result.remove(n)
+    result.add(result.size / 2, n)
 
     return result
 }
