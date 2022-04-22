ab8c6ccec3f121c141071c62dbb214339a123ed8
Artiom Bozieac <artiom.bozieac@crunchyroll.com>
2022-04-22 23:44:48 +0300

    Added comments

--- src/test/kotlin/problem.kt
+++ src/test/kotlin/problem.kt
@@ -4,6 +4,11 @@ fun main() {
     println(getAllPairs(2))
 }
 
+/**
+ * Get all pairs from 0 to n
+ * @param n - Upper boundary
+ * @return List of pairs with numbers from 0 to n
+ */
 fun getAllPairs(n: Int): List<Pair<Int, Int>> {
     var result = mutableListOf<Pair<Int, Int>>()
 
