d1418c5348771783157251b6054da1a65a9b02b0
Artiom Bozieac <artiom.bozieac@crunchyroll.com>
2022-04-22 23:37:38 +0300

    Added comments

--- src/test/kotlin/problem.kt
+++ src/test/kotlin/problem.kt
@@ -4,6 +4,11 @@ fun main() {
     println(countDown(5))
 }
 
+/**
+ * Count from n down to 0
+ * @param n - lower boundary
+ * @return List with integers from n to 0
+ */
 fun countDown(n: Int): List<Int> {
     var result = mutableListOf<Int>()
 
