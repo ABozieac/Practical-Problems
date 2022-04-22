93ff4c7dc05a8dce74e18518948b8a805bb3fdbb
Artiom Bozieac <artiom.bozieac@crunchyroll.com>
2022-04-22 23:20:44 +0300

    Added comments

--- src/test/kotlin/problem.kt
+++ src/test/kotlin/problem.kt
@@ -4,13 +4,20 @@ fun main() {
     println(maxChar("apple 1231111"))
 }
 
+/**
+ * Finds the character that is met the most often in a string
+ * @param str - String to find the character
+ * @return The character met most often
+ */
 fun maxChar(str: String): Char {
     var map = mutableMapOf<Char, Int>()
 
+    //Put every char from string in a map
     for (ch in str) {
         map.put(ch, 0)
     }
 
+    //Count the number of times every character is met
     for (i in str.indices) {
         map.put(str[i], map.getValue(str[i]) + 1)
     }
@@ -18,6 +25,7 @@ fun maxChar(str: String): Char {
     var maxValue = 0
     var maxKey = ' '
 
+    //Find the character met most often
     for (pair in map) {
         if (pair.component2() > maxValue) {
             maxValue = pair.component2()
