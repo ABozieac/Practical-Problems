0b804943ef0b33d0cd8749ee7ae701cb84ef9226
Artiom Bozieac <artiom.bozieac@crunchyroll.com>
2022-04-22 23:16:30 +0300

    Added comments

--- src/test/kotlin/problem.kt
+++ src/test/kotlin/problem.kt
@@ -1,17 +1,28 @@
 fun main() {
-    println(anagrams("rail saflty", "fairy tales"));
+    println(anagrams("rail safety", "fairy tales"));
 }
 
-fun anagrams(_str1: String, _str2: String): Boolean {
-    return (selectLetters(_str1).toCharArray().sorted() == selectLetters(_str2).toCharArray().sorted())
+/**
+ * Checks if a word is anagram of another
+ * @param str1 - First word
+ * @param str2 - Second word
+ * @return If second word is anagram of first one
+ */
+fun anagrams(str1: String, str2: String): Boolean {
+    return (selectLettersAndSort(str1) == selectLettersAndSort(str2))
 }
 
-fun selectLetters(str: String): String {
+/**
+ * Selects only letters from the string and sorts them
+ * @param str - String to select letters from
+ * @return A sorted array of letters
+ */
+fun selectLettersAndSort(str: String): List<Char> {
     val result = StringBuilder()
 
     for (i in str.indices)
         if (str[i].isLetter())
             result.append(str[i].lowercaseChar())
 
-    return result.toString()
+    return result.toString().toCharArray().sorted()
 }
