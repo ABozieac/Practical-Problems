12c9ff30e9c7693a4a5ee5980272158dddda6c44
Artiom Bozieac <artiom.bozieac@crunchyroll.com>
2022-04-22 23:21:59 +0300

    Added comments

--- src/test/kotlin/problem.kt
+++ src/test/kotlin/problem.kt
@@ -4,6 +4,12 @@ fun main() {
     println(vowels("unadouaUNADOUA"))
 }
 
+/**
+ * Counts the number of vowels in a string
+ * @param str - String to find vowels from
+ * @return the number of vowels
+ */
 fun vowels(str: String): Int {
+    //Counting vowels using regex
     return "[aeyuioAEYUIO]".toRegex().findAll(str).count()
 }
