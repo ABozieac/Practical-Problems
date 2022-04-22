ca4e395584b68670783d1129400353e4520a9c9f
Artiom Bozieac <artiom.bozieac@crunchyroll.com>
2022-04-22 23:11:22 +0300

    Added comments

--- src/test/kotlin/problem.kt
+++ src/test/kotlin/problem.kt
@@ -3,6 +3,11 @@ fun main() {
     println(longestWord("this is a house"))
 }
 
+/**
+ * Finds the longest word in a sentence
+ * @param str - Sentence
+ * @return The longest word
+ */
 fun longestWord(str: String): String {
     var words = str.split(" ")
     var biggestWord = words[0]
