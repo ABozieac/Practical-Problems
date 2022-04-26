1a76b1c0db2bd15713007e4903450c64b3e6f530
Artiom Bozieac <artiom.bozieac@crunchyroll.com>
2022-04-26 22:16:42 +0300

    Used regex instead of if

--- src/test/kotlin/problem.kt
+++ src/test/kotlin/problem.kt
@@ -1,29 +1,25 @@
 //Test for commit
 
 fun main() {
-    val str = "Artiom a vizitat, Le-notre dam"
+    val str = "aa Aa AA aA"
     println(capitalizeSentences(str))
 }
 
 /**
- * Capitalizes every word in a sentence
- * @param str - Sentence to capitalize words
+ * Capitalizes every word in a string
+ * @param str - String to capitalize words
  * @return String where all words are capitalized
  */
 fun capitalizeSentences(str: String): String {
-    var result = StringBuilder()
+    val result = StringBuilder()
 
-    for (i in str.indices) {
-        // If first letter of word or letter after symbols then capitalize it
-        if (str[i].isLetter() && i == 0 || str[i].isLetter() && !str[i - 1].isLetter() && str[i - 1] != '-')
-            result.append(str[i].uppercaseChar())
-        // Elif not first letter but capitalized then make it lowercase
-        else if (str[i].isLetter() && i != 0 && !str[i].isUpperCase())
-            result.append(str[i].lowercaseChar())
-        else
-        // Remove multiple empty spaces
-            if (i - 1 > 0 && str[i - 1] != ' ')
-                result.append(str[i])
+    //Find all words
+    val matches = Regex("[a-z[;.,'\"/?!-]A-Z]+").findAll(str)
+
+    for (match in matches){
+        result.append(match.value[0].uppercaseChar())
+        result.append(match.value.substring(1, match.value.length))
+        result.append(" ")
     }
 
     return result.toString()
